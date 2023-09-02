1ba1111caab614e793939c280d10e5ea080ff489
jafarshdq29 <143859799+jafarshdq29@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    file upload
    selebew

---
+++ lati.py
@@ -0,0 +1,32 @@
+print("nama saya jafar")
+print("nama saya jafar")
+
+
+print(nama_saya, "bertipe",type(nama_saya))
+
+
+
+
+menikah = False
+print("menikah?" ,menikah, "bertipe", type(menikah))
+
+matkul_diambil = ("alpro","sig")
+print("matkul : ",matkul_diambil2 "bertipe",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(17 / 3)
+# print(17 // 3)
+# print(17 % 3)
